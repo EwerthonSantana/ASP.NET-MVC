dbf59ebc2d638eaab8412757cd5f98146da1a211
Ewerthon Santana <ewerthon32santana@gmail.com>
2021-10-23 15:51:15 -0300

    first commit

---
+++ Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CursoMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo descrição é obrigatório")]
+        public string Descricao { get; set; }
+        [Range(1, 10, ErrorMessage = "Valor fora da faixa permitida!")]
+        public int Quantidade { get; set; }
+        public int CategoriaID { get; set; }
+        public Categoria Categoria { get; set; }
+
+    }
+}
